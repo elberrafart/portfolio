d95a55082c9c94117e5456c8947f8f69b0622a2b
razrafart <user@email.com>
2023-12-27 09:41:53 -0800

    button now turns on dark mode and also reverts to light mode

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const lightBtn = document.getElementById('light-btn')
 
-function darkMode(){
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
-    document.getElementById('myHeader').style.backgroundColor = 'black';
-    document.getElementById('myHeader').style.color = 'white';
+function toggleTheme() {
+    if (document.body.style.backgroundColor === 'black') {
+        // Switch to light mode
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        document.getElementById('myHeader').style.backgroundColor = 'white';
+    } else {
+        // Switch to dark mode
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        document.getElementById('myHeader').style.backgroundColor = 'black';
+    }
 }
 
-lightBtn.addEventListener('click', function(){
-    console.log('you clicked it')
-    darkMode()
-})
+lightBtn.addEventListener('click', function() {
+    console.log('You clicked it');
+    toggleTheme();
+});
--- resume/app.js
+++ resume/app.js
@@ -1,11 +1,20 @@
 const lightBtn = document.getElementById('light-btn')
 
-function darkMode(){
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
-    document.getElementById('myHeader').style.backgroundColor = 'black';
+function toggleTheme() {
+    if (document.body.style.backgroundColor === 'black') {
+        // Switch to light mode
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        document.getElementById('myHeader').style.backgroundColor = 'white';
+    } else {
+        // Switch to dark mode
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        document.getElementById('myHeader').style.backgroundColor = 'black';
+    }
 }
 
-lightBtn.addEventListener('click', function(){
-    darkMode()
-})
+lightBtn.addEventListener('click', function() {
+    console.log('You clicked it');
+    toggleTheme();
+});
